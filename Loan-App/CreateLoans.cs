a80c2df53ec3ab10a53273dbeaa3ad18a52ca879
Chandri0406 <chan.brey@gmail.com>
2023-08-15 21:54:31 +0200

    All needed classes are added

---
+++ Loan-App/CreateLoans.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loan_App
+{
+    internal class CreateLoans
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
