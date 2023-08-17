c2601e6313fef107562b45a6476fe7d436b91714
WobbieRessels <peter.jnr1@gmail.com>
2023-08-17 13:19:09 +0200

    creating loan class, unsure about interface implementation

--- Loan-App/Loan.cs
+++ Loan-App/Loan.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Loan_App
 {
-    internal class Loan
+    public abstract class Loan
     {
+        string custFirstname, custLastname;
+        int loanNumber, term;
+        double loanAmount;
+
+        public Loan()
+        {
+            term = 1;
+        }
+
+        protected Loan(string custFirstname, string custLastname, int loanNumber, int term, double loanAmount)
+        {
+            this.CustFirstname = custFirstname;
+            this.CustLastname = custLastname;
+            this.LoanNumber = loanNumber;
+            this.Term = term;
+            this.LoanAmount = loanAmount;
+        }
+
+        public string CustFirstname { get => custFirstname; set => custFirstname = value; }
+        public string CustLastname { get => custLastname; set => custLastname = value; }
+        public int LoanNumber { get => loanNumber; set => loanNumber = value; }
+        public int Term { get => term; set => term = value; }
+        public double LoanAmount { get => loanAmount; set => loanAmount = value; }
+
+        public override string ToString()
+        {
+            return $"A customer named: {custFirstname} {custLastname} has loan number: {loanNumber} with a loan amount of: R{loanAmount}, an interest rate of: %, and a loan term of: {term} years.";
+        }
     }
 }
