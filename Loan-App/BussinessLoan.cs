51cf5dc3b9d617ca8058500e0022e9bf117f035e
Kelo Letsoalo <keloletsoalo14@gmail.com>
2023-08-26 18:52:51 +0200

    hopefully this is in the right branch

--- Loan-App/BussinessLoan.cs
+++ Loan-App/BussinessLoan.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Loan_App
 {
-    internal class BussinessLoan
-    {
+    internal class BussinessLoan : Loan
+    {   
+
+        private double businesInterestrate ;
+        private string businessName;
+
+        public BussinessLoan() 
+        {
+        
+        }
+        public BussinessLoan(string businessName, string custFirstname, string custLastname, int loanNumber, int term, double loanAmount, double interestRate) : base(custFirstname, custLastname, loanNumber, term, loanAmount , interestRate)
+        {
+            this.BusinesInterestrate = InterestRate + 1; // instructions : "BusinessLoan constructor sets the interest rate to 1% more than the current prime interest rate. "
+            this.BusinessName = businessName;
+        }
+        
+        public double BusinesInterestrate { get => businesInterestrate; set => businesInterestrate = value; }
+        public string BusinessName { get => businessName; set => businessName = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
