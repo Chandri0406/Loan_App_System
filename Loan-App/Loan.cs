029a21fe4c81256ec746844339c041a397373221
Chandri0406 <chan.brey@gmail.com>
2023-08-27 01:26:43 +0200

    All good for now

--- Loan-App/Loan.cs
+++ Loan-App/Loan.cs
@@ -10,22 +10,14 @@ namespace Loan_App
     {
         private string custFirstname, custLastname, term;
         private int loanNumber;
-<<<<<<< HEAD
-        private double loanAmount, interestRate;
-=======
         private double loanAmount, primeInterestRate;
->>>>>>> 3cda6e6f82701d8301f4b132c27697e35cc0fb9e
 
         public Loan()
         {
             
         }
 
-<<<<<<< HEAD
-        protected Loan(string custFirstname, string custLastname, int loanNumber, string term, double loanAmount)
-=======
         public Loan(string custFirstname, string custLastname, string term, int loanNumber, double loanAmount, double primeInterestRate)
->>>>>>> 3cda6e6f82701d8301f4b132c27697e35cc0fb9e
         {
             this.CustFirstname = custFirstname;
             this.CustLastname = custLastname;
@@ -40,16 +32,12 @@ namespace Loan_App
         public string CustLastname { get => custLastname; set => custLastname = value; }
         public string Term { get => term; set => term = value; }
         public int LoanNumber { get => loanNumber; set => loanNumber = value; }
-<<<<<<< HEAD
-        public string Term { get => term; set => term = value; }
-=======
->>>>>>> 3cda6e6f82701d8301f4b132c27697e35cc0fb9e
         public double LoanAmount { get => loanAmount; set => loanAmount = value; }
         public double InterestRate { get => primeInterestRate; set => primeInterestRate = value; }
 
         public override string ToString()
         {
-            return $"A customer named: {custFirstname} {custLastname} has loan number: {loanNumber} with a loan amount of: R{loanAmount}, an interest rate of: %, and a loan term of: {term} years.";
+            return $"A customer name: \n{custFirstname} {custLastname} \nLoan number: {loanNumber} \nloan amount of: R{loanAmount} \nInterest rate of: % \nLoan term: {term}";
         }
     }
 }
