55b39f5c19c8546f365c7d1844dc2b9f9a2c638c
WobbieRessels <peter.jnr1@gmail.com>
2023-08-29 12:34:06 +0200

    fixed
    Fixed the calculateloan pull request

--- Loan-App/PersonalLoan.cs
+++ Loan-App/PersonalLoan.cs
@@ -10,7 +10,7 @@ namespace Loan_App
     {
         private double personalInterestrate;
 
-        public PersonalLoan(string custFirstname, string custLastname, string term, int loanNumber, double loanAmount, double interestRate) : base(custFirstname, custLastname, term, loanNumber, loanAmount, interestRate)
+        public PersonalLoan(string custFirstname, string custLastname, string term, int loanNumber, double loanAmount, double interestRate, double termPeriod) : base(custFirstname, custLastname, term, loanNumber, loanAmount, interestRate, termPeriod)
         {
             this.PersonalInterestrate = InterestRate + 2; // instructions : "PersonalLoan constructor sets the interest rate to 2% more than the current prime interest rate. "
         }
@@ -21,12 +21,12 @@ namespace Loan_App
 
         public override double CalculateLoan()
         {
-            return base.CalculateLoan();
+            return LoanAmount * ((1 + personalInterestrate * TermPeriod) / 100);
         }
 
         public override string ToString()
         {
-            return $"A customer name: \n{CustFirstname} {CustLastname} \nLoan number: {LoanNumber} \nloan amount of: R{LoanAmount} \nInterest rate of: {personalInterestrate}% \nLoan term: {Term}";
+            return $"A customer name: \n{CustFirstname} {CustLastname} \nLoan number: {LoanNumber} \nloan amount of: R{LoanAmount} \nInterest rate of: {personalInterestrate}% \nLoan term: {Term} \nTotal amount owed at due date: R{CalculateLoan()}";
         }
     }
 
